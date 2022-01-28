c045e99348deffc7b02b32499f817bb861c782fc
bhanu3007 <98571114+bhanu3007@users.noreply.github.com>
2022-01-28 01:45:00 -1000

    #include <stdio.h> int main() { int n,x,y;
    scanf("%d",&n);
    for(x =1; x <= n; x++)
    {
    for(y =1; y <= n; y++)
    {
    if(y <= x)
    printf("*");
    }
    printf("
    ");
    }
    }

--- Galaxy6.c
+++ Galaxy6.c
@@ -1,11 +1,16 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() {
+int n,x,y;
+
+scanf("%d",&n);
+for(x =1; x <= n; x++)
 {
-    printf("*
+for(y =1; y <= n; y++)
+{
+if(y <= x)
+printf("*");
+}
+printf("
 ");
-    printf("**
-");
-    printf("***
-");
-}    
-    
+}
+}
